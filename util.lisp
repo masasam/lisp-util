0930ae805bec301644e75276ce6ec256a9947c23
Masashi Miyaura <masasam@users.noreply.github.com>
2018-01-01 18:05:52 +0900

    Add explode

--- util.lisp
+++ util.lisp
@@ -160,3 +160,31 @@
              #'(lambda (&rest args)
                  (apply #'rmapcar fn args))
              args)))
+
+(defun prompt (&rest args)
+  (apply #'format *query-io* args)
+  (read *query-io*))
+
+(defun break-loop (fn quit &rest args)
+  (format *query-io* "Entering break-loop.~%")
+  (loop
+     (let ((in (apply #'prompt args)))
+       (if (funcall quit in)
+	   (return)
+	   (format *query-io* "~A~%" (funcall fn in))))))
+
+(defun mkstr (&rest args)
+  (with-output-to-string (s)
+    (dolist (a args) (princ a s))))
+
+(defun symb (&rest args)
+  (values (intern (apply #'mkstr args))))
+
+(defun reread (&rest args)
+  (values (read-from-string (apply #'mkstr args))))
+
+(defun explode (sym)
+  (map 'list #'(lambda (c)
+                 (intern (make-string 1
+                                      :initial-element c)))
+       (symbol-name sym)))
