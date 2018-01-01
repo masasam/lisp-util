9f8a896a45125f207c6c3d03f34187c6147ee0f8
Masashi Miyaura <masasam@users.noreply.github.com>
2018-01-01 16:20:57 +0900

    Add group

--- util.lisp
+++ util.lisp
@@ -30,3 +30,12 @@
       (lst ((val (funcall fn x)))
 	   (if val (push val acc))))
     (nreverse acc)))
+
+(defun group (source n)
+  (if (zerop n) (error "zero length"))
+  (labels ((rec (source acc)
+	     (let ((rest (nthcdr n source)))
+	       (if (consp rest)
+		   (rec rest (cons (subseq source 0 n) acc))
+		   (nreverse (cons source acc))))))
+    (if source (rec source nil) nil)))
