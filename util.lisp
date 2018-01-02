0dd35b7ce887b89c6e43477e20122f583648b514
Masashi Miyaura <masasam@users.noreply.github.com>
2018-01-02 16:49:08 +0900

    Add >case

--- util.lisp
+++ util.lisp
@@ -354,3 +354,15 @@
        (or ,@(mapcar #'(lambda (c)
                          `(funcall ,fnsym ,c))
                      choices)))))
+
+(defmacro >case (expr &rest clauses)
+  (let ((g (gensym)))
+    `(let ((,g ,expr))
+       (cond ,@(mapcar #'(lambda (cl) (>casex g cl))
+                       clauses)))))
+
+(defun >casex (g cl)
+  (let ((key (car cl)) (rest (cdr cl)))
+    (cond ((consp key) `((in ,g ,@key) ,@rest))
+          ((inq key t otherwise) `(t ,@rest))
+          (t (error "bad >case clause")))))
