fca7834cb0831230beb41e9c73d8db9d7651244b
Masashi Miyaura <masasam@users.noreply.github.com>
2018-01-02 17:08:28 +0900

    Add popn

--- util.lisp
+++ util.lisp
@@ -509,3 +509,67 @@
 					   (third b))))
 			     binds))
 	  (go ,label)))))
+
+(defmacro allf (val &rest args)
+  (with-gensyms (gval)
+    `(let ((,gval ,val))
+       (setf ,@(mapcan #'(lambda (a) (list a gval))
+                       args)))))
+
+(defmacro nilf (&rest args) `(allf nil ,@args))
+
+(defmacro tf (&rest args) `(allf t ,@args))
+
+(defmacro toggle (&rest args)
+  `(progn
+     ,@(mapcar #'(lambda (a) `(toggle2 ,a))
+               args)))
+
+(define-modify-macro toggle2 () not)
+
+(define-modify-macro concf (obj) nconc)
+
+(define-modify-macro conc1f (obj)
+  (lambda (place obj)
+    (nconc place (list obj))))
+
+(define-modify-macro concnew (obj &rest args)
+  (lambda (place obj &rest args)
+    (unless (apply #'member obj place args)
+      (nconc place (list obj)))))
+
+(defmacro _f (op place &rest args)
+  (multiple-value-bind (vars forms var set access)
+      (get-setf-method place)
+    `(let* (,@(mapcar #'list vars forms)
+            (,(car var) (,op ,access ,@args)))
+       ,set)))
+
+(defmacro pull (obj place &rest args)
+  (multiple-value-bind (vars forms var set access)
+      (get-setf-method place)
+    (let ((g (gensym)))
+      `(let* ((,g ,obj)
+              ,@(mapcar #'list vars forms)
+              (,(car var) (delete ,g ,access ,@args)))
+         ,set))))
+
+(defmacro pull-if (test place &rest args)
+  (multiple-value-bind (vars forms var set access)
+      (get-setf-method place)
+    (let ((g (gensym)))
+      `(let* ((,g ,test)
+              ,@(mapcar #'list vars forms)
+              (,(car var) (delete-if ,g ,access ,@args)))
+         ,set))))
+
+(defmacro popn (n place)
+  (multiple-value-bind (vars forms var set access)
+      (get-setf-method place)
+    (with-gensyms (gn glst)
+      `(let* ((,gn ,n)
+              ,@(mapcar #'list vars forms)
+              (,glst ,access)
+              (,(car var) (nthcdr ,gn ,glst)))
+         (prog1 (subseq ,glst 0 ,gn)
+	   ,set)))))
